39f78fdb2d6e86113f5ca70fa45f1b90870595b2
Zachary Juang <zacharyjuang@gmail.com>
2016-06-22 18:17:22 +0800

    Add proper functionality

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
  * Created by zacharyjuang on 2016-06-22.
  */
 "use strict";
+module.exports = {
+  filters: {
+    style: function (text, style) {
+      return `<span style="${style}">${text}</span>`;
+    }
+  }
+};
